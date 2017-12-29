98ed352c6daa2ac296eb9c04c83e7fe0efff41a5
hisaichi5518 <hisaichi5518@gmail.com>
2017-12-29 16:36:40 +0900

    保存したよメッセージを送れるようにした

--- lib/ruboty/actions/okotoba.rb
+++ lib/ruboty/actions/okotoba.rb
@@ -13,7 +13,30 @@ module Ruboty
       end
 
       def call
-        @message.reply "oK〜〜〜"
+        if @message.from_name == "testapp"
+          return
+        end
+
+
+        p client.chat_postMessage(
+          channel: "@#{@message.from_name}",
+          text: 'ほげほげさんへのお言葉、保存しました！編集したい場合は、また投稿してね',
+          as_user: true,
+          attachments: [
+            {
+              "fallback": "Required plain-text summary of the attachment.",
+              "color": "#36a64f",
+              "author_name": @message.from_name,
+              "text": @message.body,
+              "footer": "お言葉bot"
+            }
+          ]
+        )
+      end
+
+      private
+      def client
+        @client ||= Slack::Client.new
       end
     end
   end
