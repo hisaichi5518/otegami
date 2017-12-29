04bde55635fe81dbe778ebe4af10d742279cf591
hisaichi5518 <hisaichi5518@gmail.com>
2017-12-29 15:46:53 +0900

    すべての言葉を受け取るように変更

--- lib/ruboty/handlers/okotoba.rb
+++ lib/ruboty/handlers/okotoba.rb
@@ -4,9 +4,10 @@ module Ruboty
   module Handlers
     class Okotoba < Base
       on(
-        /bot hi/,
+        /.*/,
         description: 'お言葉を受け取ります',
-        name: 'okotoba'
+        name: 'okotoba',
+        all: true
       )
 
       def okotoba(message)
