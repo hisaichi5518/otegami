707b3d988596f8a01290c70b69249adadfbb0d16
hisaichi5518 <hisaichi5518@gmail.com>
2018-01-01 00:26:32 +0900

    Githubにイシューを立てるように変更

--- lib/ruboty/actions/notify_result.rb
+++ lib/ruboty/actions/notify_result.rb
@@ -8,18 +8,27 @@ module Ruboty
       def call
         groups.each do |members|
           group = ::Otegami::Group.new(members)
+          result = ""
           members.each do |member|
-            # TODO: Github に投稿する
-            p group.opponent(member) + "さんへ"
-            p messages[member]
-            p member + "より"
+            result += <<-"EOF"
+@#{member} →@#{group.opponent(member)}
+```
+#{messages[member]}
+```
+---
+            EOF
           end
 
-          @message.reply "こんな感じでした！"
+          res = client.create_issue("hisaichi5518/otegami", "お手紙 #{Time.now}", "#{result}")
+          @message.reply "みなさん、お手紙が届きましたよ！ #{res[:html_url]}"
         end
       end
 
       private
+      def client
+        @client ||= Octokit::Client.new(access_token: ENV['GHE_TOKEN'])
+      end
+
       def groups
         data[:groups] ||= []
       end
